ffc21b38fd06ecbfbf2706a2cde95c03fba49ed6
David Weber <davidjwe33@gmail.com>
2016-09-19 15:20:22 -0700

    Added log-in capability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @member = Member.find_by_email(params[:email])
+
+    if @member && @member.authenticate(params[:password])
+      #log in (create session)
+      session[:member_id] = @member.id
+      redirect_to "/account/#{@member.id}"
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
