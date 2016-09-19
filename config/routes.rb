0d632a9fe5a629b645dbb056792b0b7aa04aa4e3
David Weber <davidjwe33@gmail.com>
2016-09-19 11:44:55 -0700

    set up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
+  get 'members/index'
+
+  get 'members/show'
+
+  get 'members/new'
+
+  get 'members/create'
+
+  get 'members/edit'
+
+  get 'members/update'
+
+  get 'members/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
